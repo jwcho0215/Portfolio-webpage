867b5a96d3ec7b7e44ebf8bca1ef29e8aa998e83
jwpark0215 <jwpark021523@outlook.com>
2026-02-05 13:35:49 +1100

    added css

---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home =  () => { 
+    return(
+        <div className="min-h-screen"></div>
+        {/* theme toggle*/}
+
+        {/* background  Effects*/}
+
+        {/* navbar*/}
+
+        {/* main page */}
+
+        {/* footer */}
+    )
+} 
