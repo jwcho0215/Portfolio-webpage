4b4fb257cbbdbaee02e0010ec8e8fd23b56e44dc
Joshua Cho <z5586662@ad.unsw.edu.au>
2026-02-19 17:13:37 +1100

    additional features projects/ others

---
+++ vite-project/src/components/Skills.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import { cn } from "../lib/utils";
+const skills = [
+  { name: "JavaScript", level: 85, categories: "frontend" },
+  { name: "React", level: 85, categories: "frontend" },
+  { name: "Python", level: 90, categories: "backend" },
+  { name: "C", level: 80, categories: "backend" },
+  { name: "Rust", level: 80, categories: "backend" },
+  { name: "Java", level: 85, categories: "backend" },
+  { name: "Spark", level: 85, categories: "data" },
+  { name: "Hadoop", level: 85, categories: "data" },
+  { name: "SQL", level: 85, categories: "data" },
+  { name: "Git/Github ", level: 90, categories: "tools" },
+];
+const categories = ["all", "frontend", "backend", "data", "tools"];
+
+export const Skills = () => {
+  const [activeCategory, setActiveCategory] = useState("all");
+  const filteredSkills = skills.filter(
+    (skill) => activeCategory === "all" || skill.categories === activeCategory,
+  );
+  return (
+    <section id="skills" className="py-24 px-4 relative bg-secondary/30">
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          My <span className="text-primary"> Skills</span>
+        </h2>
+
+        <div className="flex flex-wrap justify-center gap-4 mb-12 ">
+          {categories.map((category, key) => (
+            <button
+              key={key}
+              className={cn(
+                "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                activeCategory === category
+                  ? "bg-primary text-primary-foreground"
+                  : "bg-secondary/70 text-foreground hover:bd-secondary",
+              )}
+              onClick={() => setActiveCategory(category)}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {filteredSkills.map((skill, key) => (
+            <div
+              key={key}
+              className="bg-card p-6 rounded-lg shadow-xs card-hover"
+            >
+              <div className="text-left mb-4">
+                <h3 className="font-semibold text-lg">{skill.name}</h3>
+              </div>
+              <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden">
+                <div
+                  className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out]"
+                  style={{ width: skill.level + "%" }}
+                ></div>
+              </div>
+              <div className="text-right mt-1">
+                <span className="text-sm text-muted-foreground">
+                  {skill.level}%
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import {ThemeToggle} from "@/components/ThemeToggle"
 import {StarBackground} from "@/components/StarBackground"
 import { NavBar } from "@/components/NavBar";
-export const Home =  () => { 
+import { HeroSection } from "@/components/HeroSection";
+import { About } from "@/components/About";
+import {Skills} from "@/components/Skills";
+import { Project } from "../components/Project";
+
+export const Home =  () => {
     return(
         <div className="min-h-screen bg-background text-foreground ">
             <ThemeToggle/>
             <StarBackground/>
             <NavBar/>
+            <main>
+                <HeroSection />
+                <About/>
+                <Skills/>
+                <Project/>
+            </main>
         </div>
     )
-} 
+}
